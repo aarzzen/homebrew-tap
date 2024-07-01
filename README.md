19b1835b8b88afc55b863409ee4e86a2b1a7e6e6
dat2csv <150282876+dat2csv@users.noreply.github.com>
2024-07-01 21:43:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Dat2csv Tap
+
+## How do I install these formulae?
+
+`brew install dat2csv/tap/<formula>`
+
+Or `brew tap dat2csv/tap` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
---
+++ cmd/dat2csv.rb
@@ -0,0 +1,17 @@
+class Dat2csv < Formula
+  desc "Simple utility to convert DAT to CSV"
+  homepage "https://github.com/dat2csv/dat2csv"
+  license "MIT"
+
+  if Hardware::CPU.arm?
+    url "https://github.com/dat2csv/dat2csv/releases/download/0.9.1/dat2csv-darwin-arm64.tar.gz"
+    sha256 "b7e74060f898b7e8c3b58ab4b081effd87311e294cbfe920b06117ccefb3da62"
+  else
+    url "https://github.com/dat2csv/dat2csv/releases/download/0.9.1/dat2csv-darwin-amd64.tar.gz"
+    sha256 "e07dacfb36b91b10ba897fe3840e04aeba1a556e484b1f00a81b7b1c2929f9cc"
+  end
+
+  def install
+    bin.install "dat2csv"
+  end
+end
